f57bb23c039d76f30caae3d5d4614b0a2e5e474b
Antonio Abilio Parada Santos <up202205469@fe.up.pt>
2024-05-08 18:22:42 +0100

    video for project

---
+++ proj/src/device_controllers/video/video.h
@@ -0,0 +1,15 @@
+#include <lcom/lcf.h>
+#include "video_macros.h"
+
+void returnToTextMode();
+int video_get_mode_information(uint16_t mode);
+int video_map_vram();
+int video_set_graphics_mode(uint16_t mode);
+unsigned int video_get_phys_addr();
+unsigned int video_get_vram_size();
+int drawPixel(uint16_t x, uint16_t y, uint32_t color);
+int (vg_draw_hline)(uint16_t x, uint16_t y, uint16_t len, uint32_t color);
+int (vg_draw_rectangle)(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint32_t color);
+int vg_draw_pattern(uint8_t no_rectangles, uint32_t first, uint8_t step);
+int vg_draw_pixmap(uint16_t x, uint16_t y, uint8_t* map, xpm_image_t pixmap_and_metadata);
+void vg_clear_screen();
---
+++ proj/src/device_controllers/video/video_macros.h
@@ -0,0 +1,16 @@
+#ifndef _LCOM_VIDEO_MACROS_H_
+#define _LCOM_VIDEO_MACROS_H_
+
+// Video BIOS Service
+#define VIDEO_BIOS_SERVICE 0x10 
+
+// Video functions
+#define VBE_MODE_INFO   0x01
+#define VBE_SET_MODE    0x02
+
+#define VBE_CALL 0x4f // VBE call, function 02 - set VBE mode
+
+// Graphics modes
+#define VIDEO_LINEAR_FRAMEBUF_MODE BIT(14)
+
+#endif /* _LCOM_VIDEO_MACROS_H */
