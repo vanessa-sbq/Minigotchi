4865d3c892fa44fd8512ee5028e5d6ae915c0f09
JoAPSo Pedro Monteiro de Castro <up202206575@fe.up.pt>
2024-05-30 16:55:58 +0000

    Upload New File

---
+++ lab7/test7.c
@@ -0,0 +1,25 @@
+#include "test7.h"
+/*
+int ser_test_conf(unsigned short base_addr) {
+
+}
+
+int ser_test_set(unsigned short base_addr, unsigned long bits, unsigned long stop, 
+	           long parity, unsigned long rate) { 
+
+}
+
+int ser_test_poll(unsigned short base_addr, unsigned char tx, unsigned long bits, 
+                    unsigned long stop, long parity, unsigned long rate, 
+                    int stringc, char *strings[]) {
+
+}
+
+int ser_test_int() { 
+
+}
+
+int ser_test_fifo() {
+
+}
+*/
